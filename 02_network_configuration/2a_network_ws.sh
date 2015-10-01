c738b8f8464f9d1350c4653f8586abbc0a449ae2
tiiiecherle <tiiiecherle@gmail.com>
2015-10-01 23:26:11 +0200

    more network user profiles

---
+++ 02_network_configuration/2a_network_ws.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+# manpage
+# https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man8/networksetup.8.html
+
+# to reset all the network settings completely do
+#sudo rm -rf /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist
+#sudo rm -rf /Library/Preferences/SystemConfiguration/com.apple.network.identification.plist
+#sudo rm -rf /Library/Preferences/SystemConfiguration/com.apple.wifi.message-tracer.plist
+#sudo rm -rf /Library/Preferences/SystemConfiguration/NetworkInterfaces.plist
+#sudo rm -rf /Library/Preferences/SystemConfiguration/preferences.plist
+#sudo reboot
+
+# a few commands
+# networksetup -detectnewhardware
+# networksetup -listallhardwareports
+# networksetup -listallnetworkservices
+# networksetup -listlocations
+
+# asking for the administrator password upfront
+sudo -v
+
+# keep-alive: update existing 'sudo' time stamp until script is finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# deleting all network locations
+echo please ignore error about missing preferences.plist file, it will be created automatically
+sudo rm -rf /Library/Preferences/SystemConfiguration/preferences.plist
+
+# creating new location automatic
+echo adding location automatic
+sudo networksetup -createlocation "Automatisch" populate
+sleep 3
+sudo networksetup -switchtolocation "Automatisch"
+sleep 3
+
+# creating new location office_lan
+echo adding location office_lan
+sudo networksetup -createlocation "office_lan"
+sleep 3
+sudo networksetup -switchtolocation "office_lan"
+sleep 3
+sudo networksetup -createnetworkservice "Ethernet" "Ethernet"
+sleep 3
+sudo networksetup -setmanual "Ethernet" 192.168.1.6 255.255.255.0 192.168.1.1
+sleep 3
+sudo networksetup -setdnsservers "Ethernet" 192.168.1.1
+sleep 3
+
+# deleting created preferences backup file
+sleep 3
+sudo rm -rf /Library/Preferences/SystemConfiguration/preferences.plist.old
+
+# echo script finished
+echo "all network locations created ;)"
+
+# changing to automatic location
+echo "changing to location automatic" 
+sudo networksetup -switchtolocation "Automatisch"
+echo "done ;)" 
