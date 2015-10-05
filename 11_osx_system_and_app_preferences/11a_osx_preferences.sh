275f71f5f4c6040cd50b827058bd0a705b1f20ab
tiiiecherle <tiiiecherle@gmail.com>
2015-10-05 11:49:46 +0200

    renaming

--- 11_osx_system_and_app_preferences/11a_osx_preferences.sh
+++ 11_osx_system_and_app_preferences/11a_osx_preferences.sh
@@ -506,15 +506,37 @@ defaults write "/Library/Application Support/CrashReporter/DiagnosticMessagesHis
 
 echo "preferences spotlight"
 
-# disable spotlight indexing for any volume that gets mounted and has not yet been indexed before.
-#sudo mdutil -i off "/Volumes/VOLUMENAME"
-#sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
-
-# stop indexing for some volumes which will not be indexed again
-sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes/office" "/Volumes/extra" "/Volumes/scripts"
-
-# check entries
-#sudo defaults read /.Spotlight-V100/VolumeConfiguration Exclusions
+# change indexing order and disable some search results
+# yosemite (and newer) specific search results
+# 	MENU_DEFINITION
+# 	MENU_CONVERSION
+# 	MENU_EXPRESSION
+# 	MENU_SPOTLIGHT_SUGGESTIONS (send search queries to Apple)
+# 	MENU_WEBSEARCH             (send search queries to Apple)
+# 	MENU_OTHER
+defaults write com.apple.spotlight orderedItems -array \
+'{"enabled" = 1;"name" = "APPLICATIONS";}' \
+'{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+'{"enabled" = 1;"name" = "DIRECTORIES";}' \
+'{"enabled" = 1;"name" = "PDF";}' \
+'{"enabled" = 0;"name" = "FONTS";}' \
+'{"enabled" = 1;"name" = "DOCUMENTS";}' \
+'{"enabled" = 1;"name" = "MESSAGES";}' \
+'{"enabled" = 1;"name" = "CONTACT";}' \
+'{"enabled" = 1;"name" = "EVENT_TODO";}' \
+'{"enabled" = 1;"name" = "IMAGES";}' \
+'{"enabled" = 1;"name" = "BOOKMARKS";}' \
+'{"enabled" = 1;"name" = "MUSIC";}' \
+'{"enabled" = 1;"name" = "MOVIES";}' \
+'{"enabled" = 1;"name" = "PRESENTATIONS";}' \
+'{"enabled" = 1;"name" = "SPREADSHEETS";}' \
+'{"enabled" = 0;"name" = "SOURCE";}' \
+'{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
+'{"enabled" = 0;"name" = "MENU_OTHER";}' \
+'{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
+'{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
+'{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
+'{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
 
 # another way of stopping indexing AND searching for a volume (very helpful for network volumes) is putting an empty file ".metadata_never_index"
 # in the root directory of the volume and run "mdutil -E /Volumes/*" afterwards, check if it worked with sudo mdutil -s /Volumes/*
@@ -539,49 +561,27 @@ sudo rm -rf /.Spotlight-V100/Store*
 #sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
 #sudo mdutil -i on /Volumes/*
 
+# deleting and reindexing all turned on (mdutil -i) volumes
+sudo mdutil -E /Volumes/*
+
 # only turn on indexing of the local harddrive named "macintosh_hd"
 sudo mdutil -i on /Volumes/macintosh_hd
 
-# deleting and reindexing all turned on (mdutil -i) volumes
-sudo mdutil -E /Volumes/*
+# disable spotlight indexing for any volume that gets mounted and has not yet been indexed before.
+#sudo mdutil -i off "/Volumes/VOLUMENAME"
+#sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
+
+# stop indexing for some volumes which will not be indexed again
+sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes/office" "/Volumes/extra" "/Volumes/scripts"
+
+# check entries
+#sudo defaults read /.Spotlight-V100/VolumeConfiguration Exclusions
 
 # checking status of volumes
 sudo mdutil -s /Volumes/*
 
 # disabling lookup / spotlight suggestions
 /usr/libexec/PlistBuddy -c "Add lookupEnabled:suggestionsEnabled bool false" ~/Library/Preferences/com.apple.lookup.plist
-
-# change indexing order and disable some search results
-# yosemite (and newer) specific search results
-# 	MENU_DEFINITION
-# 	MENU_CONVERSION
-# 	MENU_EXPRESSION
-# 	MENU_SPOTLIGHT_SUGGESTIONS (send search queries to Apple)
-# 	MENU_WEBSEARCH             (send search queries to Apple)
-# 	MENU_OTHER
-defaults write com.apple.spotlight orderedItems -array \
-'{"enabled" = 1;"name" = "APPLICATIONS";}' \
-'{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
-'{"enabled" = 1;"name" = "DIRECTORIES";}' \
-'{"enabled" = 1;"name" = "PDF";}' \
-'{"enabled" = 1;"name" = "FONTS";}' \
-'{"enabled" = 1;"name" = "DOCUMENTS";}' \
-'{"enabled" = 0;"name" = "MESSAGES";}' \
-'{"enabled" = 0;"name" = "CONTACT";}' \
-'{"enabled" = 0;"name" = "EVENT_TODO";}' \
-'{"enabled" = 0;"name" = "IMAGES";}' \
-'{"enabled" = 0;"name" = "BOOKMARKS";}' \
-'{"enabled" = 0;"name" = "MUSIC";}' \
-'{"enabled" = 0;"name" = "MOVIES";}' \
-'{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-'{"enabled" = 0;"name" = "SPREADSHEETS";}' \
-'{"enabled" = 0;"name" = "SOURCE";}' \
-'{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
-'{"enabled" = 0;"name" = "MENU_OTHER";}' \
-'{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
-'{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
-'{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
-'{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
 
 
 
