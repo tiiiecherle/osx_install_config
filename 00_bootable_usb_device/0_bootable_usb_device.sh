a87e289914c74580d00bb253db580d139858dc8d
tiiiecherle <tiiiecherle@gmail.com>
2016-05-30 15:03:36 +0200

    small description update

--- 00_bootable_usb_device/0_bootable_usb_device.sh
+++ 00_bootable_usb_device/0_bootable_usb_device.sh
@@ -2,8 +2,8 @@
 
 # format usb drive with guid partition table
 # format partition with OS X Extended (Journaled) and name it "Untitled" and leave it mounted
-# download installer to some directory and put the POSIX PAth in the variable
-# adjust installer name and run the following command in terminal and enter admin password
+# download installer to some directory and put the POSIX path in the variable
+# adjust installer path and name and run the following command in terminal and enter admin password
 
 INSTALLERPATH="/Applications/Install OS X El Capitan.app"
 VOLUMEPATH="/Volumes/Untitled"
