c4cc59980e38c7e86c9842160dd910c40ecae36b
tiiiecherle <tiiiecherle@gmail.com>
2018-05-26 08:35:25 +0200

    better logged in user name and shutdown at end of script

--- 09_launchd/9d_run_on_logout/install/run_on_logout.sh
+++ 09_launchd/9d_run_on_logout/install/run_on_logout.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 # the script is run as root
-# that`s why $USER will not work, use $(logname) for the current logged in user instead
-# every command that does not have sudo -u $(logname) upfront has to be run as root, but sudo is not needed here
+# that`s why $USER will not work, use the current logged in user instead
+# every command that does not have sudo -u $loggedInUser upfront has to be run as root, but sudo is not needed here
+
+# getting logged in user
+#/bin/ls -l /dev/console | /usr/bin/awk '{ print $3 }'
+#stat -f%Su /dev/console
+#defaults read /Library/Preferences/com.apple.loginwindow.plist lastUserName
+# recommended way
+loggedInUser=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+echo "loggedInUser is $loggedInUser..."
 
 run_on_logout () {
 	
 	# cleaning caches
 	rm -rf /Library/Caches/* &
 	rm -rf /System/Library/Caches/* &
-	sudo -u $(logname) rm -rf /Users/$(logname)/Library/Caches/* &
+	sudo -u $loggedInUser rm -rf /Users/$loggedInUser/Library/Caches/* &
 	
 	# cleaning dnscaches
 	dns_caches () {
@@ -19,25 +27,25 @@ run_on_logout () {
 	dns_caches &
 	
 	# cleaning safari
-	sudo -u $(logname) rm -f /Users/$(logname)/Library/Safari/History.db* &
-	sudo -u $(logname) rm -f /Users/$(logname)/Library/Safari/LastSession.plist &
-	sudo -u $(logname) rm -f /Users/$(logname)/Library/Safari/Downloads.plist &
-	sudo -u $(logname) rm -f /Users/$(logname)/Library/Safari/KnownSitesUsingPlugIns.plist &
-	sudo -u $(logname) rm -f /Users/$(logname)/Library/Safari/Downloads.plist &
-	sudo -u $(logname) rm -f /Users/$(logname)/Library/Safari/TopSites.plist &
-	#sudo -u $(logname) rm -rf /Users/$(logname)/Library/Safari/LocalStorage
-	find /Users/$(logname)/Library/Safari/LocalStorage/* -type f -not -name "*.nba.*" -print0 | xargs -0 rm -f &
-	sudo -u $(logname) rm -rf /Users/$(logname)/Library/Safari/Databases &
-	sudo -u $(logname) rm -rf "/Users/$(logname)/Library/Safari/Favicon Cache" &
-	sudo -u $(logname) rm -rf "/Users/$(logname)/Library/Safari/Template Icons" &
-	sudo -u $(logname) rm -rf "/Users/$(logname)/Library/Safari/Touch Icons Cache" &
-	# rest is already deleted from /Users/$(logname)/Library/Caches/*
+	sudo -u $loggedInUser rm -f /Users/$loggedInUser/Library/Safari/History.db* &
+	sudo -u $loggedInUser rm -f /Users/$loggedInUser/Library/Safari/LastSession.plist &
+	sudo -u $loggedInUser rm -f /Users/$loggedInUser/Library/Safari/Downloads.plist &
+	sudo -u $loggedInUser rm -f /Users/$loggedInUser/Library/Safari/KnownSitesUsingPlugIns.plist &
+	sudo -u $loggedInUser rm -f /Users/$loggedInUser/Library/Safari/Downloads.plist &
+	sudo -u $loggedInUser rm -f /Users/$loggedInUser/Library/Safari/TopSites.plist &
+	#sudo -u $loggedInUser rm -rf /Users/$loggedInUser/Library/Safari/LocalStorage
+	find /Users/$loggedInUser/Library/Safari/LocalStorage/* -type f -not -name "*.nba.*" -print0 | xargs -0 rm -f &
+	sudo -u $loggedInUser rm -rf /Users/$loggedInUser/Library/Safari/Databases &
+	sudo -u $loggedInUser rm -rf "/Users/$loggedInUser/Library/Safari/Favicon Cache" &
+	sudo -u $loggedInUser rm -rf "/Users/$loggedInUser/Library/Safari/Template Icons" &
+	sudo -u $loggedInUser rm -rf "/Users/$loggedInUser/Library/Safari/Touch Icons Cache" &
+	# rest is already deleted from /Users/$loggedInUser/Library/Caches/*
 	
 	# cleaning firefox storage
-	FIREFOX_PROFILES="/Users/$(logname)/Library/Application Support/Firefox/Profiles"
+	FIREFOX_PROFILES="/Users/$loggedInUser/Library/Application Support/Firefox/Profiles"
 	if [[ -e "$FIREFOX_PROFILES" ]]
 	then 
-		sudo -u $(logname) rm -rf "$FIREFOX_PROFILES"/*.default/storage/* &
+		sudo -u $loggedInUser rm -rf "$FIREFOX_PROFILES"/*.default/storage/* &
 	else
 		:
 	fi
@@ -46,9 +54,9 @@ run_on_logout () {
 	PSD_INSTALLATION="/Applications/Progressive Downloader.app"
 	if [[ -e "$PSD_INSTALLATION" ]]
 	then
-		sudo -u $(logname) rm -rf "/Users/$(logname)/Library/Application Support/Progressive Downloader Data" &
-		# has to be run as user (sudo -u $(logname)) or permissions would change and file would no longer work
-		sudo -u $(logname) defaults delete /Users/$(logname)/Library/Preferences/com.PS.PSD.plist psDownloadedBytes &
+		sudo -u $loggedInUser rm -rf "/Users/$loggedInUser/Library/Application Support/Progressive Downloader Data" &
+		# has to be run as user (sudo -u $loggedInUser) or permissions would change and file would no longer work
+		sudo -u $loggedInUser defaults delete /Users/$loggedInUser/Library/Preferences/com.PS.PSD.plist psDownloadedBytes &
 	else
 		:
 	fi
@@ -56,4 +64,13 @@ run_on_logout () {
 }
 
 run_on_logout
-wait
+
+TIME_OUT=0
+while [[ $TIME_OUT -lt 10 ]]
+do
+    sleep 1
+    TIME_OUT=$((TIME_OUT+1))
+done
+# wait
+
+shutdown -h now
