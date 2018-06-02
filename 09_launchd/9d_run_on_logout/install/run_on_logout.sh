3ffbde2a51bdb956ad299644d1919eb1bb115caa
tiiiecherle <tiiiecherle@gmail.com>
2018-06-02 11:52:09 +0200

    added parallel and non-parallel option and disabled shutdown command due to preventing reboots

--- 09_launchd/9d_run_on_logout/install/run_on_logout.sh
+++ 09_launchd/9d_run_on_logout/install/run_on_logout.sh
@@ -12,7 +12,7 @@
 loggedInUser=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
 echo "loggedInUser is $loggedInUser..."
 
-run_on_logout () {
+run_on_logout_parallel () {
 	
 	# cleaning caches
 	rm -rf /Library/Caches/* &
@@ -63,14 +63,78 @@ run_on_logout () {
 
 }
 
-run_on_logout
+run_on_logout () {
+	
+	# cleaning caches
+	rm -rf /Library/Caches/*
+	rm -rf /System/Library/Caches/*
+	sudo -u $loggedInUser rm -rf /Users/$loggedInUser/Library/Caches/*
+	
+	# cleaning dnscaches
+	dns_caches () {
+	dscacheutil -flushcache
+	killall -HUP mDNSResponder
+	}
+	dns_caches
+	
+	# cleaning safari
+	sudo -u $loggedInUser rm -f /Users/$loggedInUser/Library/Safari/History.db*
+	sudo -u $loggedInUser rm -f /Users/$loggedInUser/Library/Safari/LastSession.plist
+	sudo -u $loggedInUser rm -f /Users/$loggedInUser/Library/Safari/Downloads.plist &
+	sudo -u $loggedInUser rm -f /Users/$loggedInUser/Library/Safari/KnownSitesUsingPlugIns.plist
+	sudo -u $loggedInUser rm -f /Users/$loggedInUser/Library/Safari/Downloads.plist
+	sudo -u $loggedInUser rm -f /Users/$loggedInUser/Library/Safari/TopSites.plist
+	#sudo -u $loggedInUser rm -rf /Users/$loggedInUser/Library/Safari/LocalStorage
+	find /Users/$loggedInUser/Library/Safari/LocalStorage/* -type f -not -name "*.nba.*" -print0 | xargs -0 rm -f
+	sudo -u $loggedInUser rm -rf /Users/$loggedInUser/Library/Safari/Databases
+	sudo -u $loggedInUser rm -rf "/Users/$loggedInUser/Library/Safari/Favicon Cache"
+	sudo -u $loggedInUser rm -rf "/Users/$loggedInUser/Library/Safari/Template Icons"
+	sudo -u $loggedInUser rm -rf "/Users/$loggedInUser/Library/Safari/Touch Icons Cache"
+	# rest is already deleted from /Users/$loggedInUser/Library/Caches/*
+	
+	# cleaning firefox storage
+	FIREFOX_PROFILES="/Users/$loggedInUser/Library/Application Support/Firefox/Profiles"
+	if [[ -e "$FIREFOX_PROFILES" ]]
+	then 
+		sudo -u $loggedInUser rm -rf "$FIREFOX_PROFILES"/*.default/storage/*
+	else
+		:
+	fi
+	
+	# cleaning progressive downloader
+	PSD_INSTALLATION="/Applications/Progressive Downloader.app"
+	if [[ -e "$PSD_INSTALLATION" ]]
+	then
+		sudo -u $loggedInUser rm -rf "/Users/$loggedInUser/Library/Application Support/Progressive Downloader Data"
+		# has to be run as user (sudo -u $loggedInUser) or permissions would change and file would no longer work
+		sudo -u $loggedInUser defaults delete /Users/$loggedInUser/Library/Preferences/com.PS.PSD.plist psDownloadedBytes
+	else
+		:
+	fi
 
-TIME_OUT=0
-while [[ $TIME_OUT -lt 10 ]]
-do
-    sleep 1
-    TIME_OUT=$((TIME_OUT+1))
-done
+}
+
+run_on_logout_parallel
+#run_on_logout
+sleep 0.5
+
+#TIME_OUT=0
+#while [[ $TIME_OUT -lt 4 ]]
+#do
+#    sleep 1
+#    TIME_OUT=$((TIME_OUT+1))
+#done
 # wait
 
-shutdown -h now
+# testing - does not help on hangs during shutdown
+#pkill -u $loggedInUser
+#sudo pkill -u root
+#sudo kill -9 $(ps A | awk '{print $1}' | tail -n +2)
+
+# do not use this here as it prevents reboots
+# I haven`t found a way yet to check on shutdown if the shutdown cause is a reboot or real shutdown
+# until then this stays disabled
+# shutdown
+#sudo shutdown -h now
+
+
