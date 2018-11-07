143ff9ffaf5448fc1b7ffff2256416105cb86c70
tiiiecherle <tiiiecherle@gmail.com>
2018-11-07 15:50:37 +0100

    renamed script

---
+++ 11_system_and_app_preferences/11b_safari extensions_and_self_singed_certificate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+### safari extensions
+# as apple changed the format of extensions for 10.14 and up it is no longer necessary to restore the "*.safariextz" files
+# "/$HOMEFOLDER/Library/Safari/Extensions/Extensions.plist"
+# "/$HOMEFOLDER/Library/Containers/com.apple.Safari/Data/Library/Preferences/com.apple.Safari.Extensions.plist"
+# are restored by the restore script if they were present at backup
+
+echo "opening safari apps that include extensions..."
+# should already be enabled by retoring ~/Library/Containers/com.apple.Safari/Data/Library/WebKit/ContentExtensions
+open "/Applications/Better.app"
+open "/Applications/GhosteryLite.app"
+
+
+### accepting self signed sever certificate
+# opening safari again to accept self signed certificate for syncing calendar, contacts and reminders on local network via https
+echo ''
+echo "please accept self-signed certificate by showing details, opening the webiste and entering the password..."
+open -a /Applications/Safari.app https://172.16.1.200	
+echo "safari has to be quit before continuing..."
+while ps aux | grep 'Safari.app' | grep -v grep > /dev/null; do sleep 1; done
+
+echo 'done ;)'
