dc4f30a79570a2ef9643b745c5115759a9e524ac
tiiiecherle <tiiiecherle@gmail.com>
2019-05-19 19:31:54 +0200

    restructured mail script

--- 13_apple_mail_and_accounts/13_mail_migrate_accounts.sh
+++ 13_apple_mail_and_accounts/13_mail_migrate_accounts.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-###
-### in 10.11 apple moves all remaining accounts from
-### ~/Library/Mail/V2/MailData/Accounts.plist to
-### ~/Library/Accounts/Accounts3.sqlite
-### if you are doing a clean install of 10.11 to update from 10.10 you need to run this script to update accounts
-### be careful of the order of the steps to take for that (see separate file)
-###
-
-# el capitan
-#sudo open /System/Library/InternetAccounts/internetAccountsMigrator
-
 # sierra
 if [ -e ~/Library/Accounts/Accounts3.sqlite ]
 then
@@ -26,6 +15,17 @@ echo "deleting files to rebuild the mailindex at next start of mail..."
 # el capitan
 if [ -e ~/Library/Mail/V3/MailData/ ]
 then
+	###
+	### in 10.11 apple moves all remaining accounts from
+	### ~/Library/Mail/V2/MailData/Accounts.plist to
+	### ~/Library/Accounts/Accounts3.sqlite
+	### if you are doing a clean install of 10.11 to update from 10.10 you need to run this script to update accounts
+	### be careful of the order of the steps to take for that (see separate file accounts_mail_calendar_contacts_el_capitan.txt)
+	###
+	
+	# el capitan
+	#sudo open /System/Library/InternetAccounts/internetAccountsMigrator
+
 	find ~/Library/Mail/V3/MailData/ -type f -name "Envelope Index*" -print0 | xargs -0 rm
 	find ~/Library/Mail/V3/MailData/ -type f -name "ExternalUpdates.*" -print0 | xargs -0 rm
 else
