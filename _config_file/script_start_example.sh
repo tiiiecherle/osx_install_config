7a5331edc0d287338c9cf1b1668d3619a826067d
tiiiecherle <tiiiecherle@gmail.com>
2019-07-09 17:41:43 +0200

    updated example

--- _config_file/script_start_example.sh
+++ _config_file/script_start_example.sh
@@ -5,8 +5,7 @@
 ###
 
 if [[ -f ~/.shellscriptsrc ]]; then . ~/.shellscriptsrc; else echo '' && echo -e '\033[1;31mshell script config file not found...\033[0m\nplease install by running this command in the terminal...\n\n\033[1;34msh -c "$(curl -fsSL https://raw.githubusercontent.com/tiiiecherle/osx_install_config/master/_config_file/install_config_file.sh)"\033[0m\n' && exit 1; fi
-eval_function() { function_to_eval="$@"; eval "$(typeset -f $function_to_eval)" && "$function_to_eval" ; }
-eval_function env_get_shell_specific_variables
+eval "$(typeset -f env_get_shell_specific_variables)" && env_get_shell_specific_variables
 
 
 
@@ -16,13 +15,24 @@ eval_function env_get_shell_specific_variables
 
 ### testing sourcing and variables
 echo ''
-TEST_SOURCING_AND_VARIABLES=yes
-if [[ -f ~/.shellscriptsrc ]]; then . ~/.shellscriptsrc; else echo '' && echo -e '\033[1;31mshell script config file not found...\033[0m\nplease install by running this command in the terminal...\n\n\033[1;34msh -c "$(curl -fsSL https://raw.githubusercontent.com/tiiiecherle/osx_install_config/master/_config_file/install_config_file.sh)"\033[0m\n' && exit 1; fi
-eval_function() { function_to_eval="$@"; eval "$(typeset -f $function_to_eval)" && "$function_to_eval" ; }
-eval_function env_get_shell_specific_variables
+echo "master shell script..."
+echo "script is sourced: $SCRIPT_IS_SOURCED"
+echo "script name is $SCRIPT_NAME"
+echo "script directory is $SCRIPT_DIR"
+echo "script directory one back is $SCRIPT_DIR_ONE_BACK"
+echo ''
+
+TEST_SOURCING_AND_VARIABLES=yes . ~/.shellscriptsrc
+eval "$(typeset -f env_get_shell_specific_variables)" && env_get_shell_specific_variables
+
+# old
+# not working from config file
+# both lines together would need to be used after sourcing the config file from another file
+#eval_function() { function_to_eval="$@"; eval "$(typeset -f $function_to_eval)" && "$function_to_eval" ; }
+#eval_function env_get_shell_specific_variables
 
 echo ''
-echo "parent shell script..."
+echo "master shell script..."
 echo "script is sourced: $SCRIPT_IS_SOURCED"
 echo "script name is $SCRIPT_NAME"
 echo "script directory is $SCRIPT_DIR"
